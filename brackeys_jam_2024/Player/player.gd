4f605c407b565070bd31155c37be65ff59528404
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2024-09-08 18:44:07 -0400

    - Player can no longer jump multiple times - Fixed a bug when switching lanes while on the ground sometimes makes us fall instead of teleporting to the other lane

--- brackeys_jam_2024/Player/player.gd
+++ brackeys_jam_2024/Player/player.gd
@@ -1,11 +1,12 @@
 extends RigidBody2D
 
 var lane: int = 1
-var down_velocity: float
+var old_lane: int = lane
 @export var lane_offset: float
-const jump_impulse = 350
-var up_pressed: bool = false
 
+const jump_impulse = 350
+@onready var ground_detector := $GroundDetector
+var is_on_ground: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	collision_mask = 1 << lane
@@ -13,17 +14,21 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("lane_down") && lane < 2:
-		position.y += lane_offset
 		lane += 1
 	if event.is_action_pressed("lane_up") && lane > 0:
-		position.y -= lane_offset
 		lane -= 1
-	if event.is_action_pressed("jump"):
+	if event.is_action_pressed("jump") && is_on_ground:
 		apply_central_impulse(Vector2.UP * jump_impulse)
+		is_on_ground = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	is_on_ground = linear_velocity.y <= 0 && ground_detector.is_colliding()
+		
 	pass
 	
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	collision_mask = 1 << lane
+	position.y += lane_offset * (lane - old_lane)
+	ground_detector.collision_mask = collision_mask
+	old_lane = lane
