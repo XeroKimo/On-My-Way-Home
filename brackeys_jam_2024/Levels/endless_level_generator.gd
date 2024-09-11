4b6b55f8a77fabfbd63d4e652b092ace78ec7d40
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2024-09-11 19:51:18 -0400

    - Lanes are now separate from the sky - A storm can now occur randomly. When a storm starts lava is spawned on off screen level parts. When it ends, all lava elements are destroyed - Added a debug way to have invincible mode

--- brackeys_jam_2024/Levels/endless_level_generator.gd
+++ brackeys_jam_2024/Levels/endless_level_generator.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var background: PackedScene
 @export var obstacles: Array
+@export var lava_obstacles: Array
 @onready var lanes = [$"Lane 1", $"Lane 2", $"Lane 3"]
 
 @export var initial_speed: float = 300
@@ -12,6 +13,14 @@ var speed: float
 var level_parts: Array
 var lane_width: float
 const parts_buffer : int = 2
+
+var storm_brewing: bool = false
+var storm_timer_seconds: float
+var previous_storm_timer_seconds: float
+@export var pre_storm_duration_seconds: float = 5
+@export var storm_duration_seconds: float = 15
+var storm_in_progress: bool:
+	get: return storm_brewing && storm_timer_seconds >= 0 && storm_timer_seconds <= storm_duration_seconds
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,18 +34,51 @@ func _ready() -> void:
 		child = background.instantiate() as EndlessLevelPart
 		add_child(child)
 		level_parts.push_back(child)
-		child.initialize([(lanes[0] as Node2D).position, (lanes[1] as Node2D).position, (lanes[2] as Node2D).position], obstacles, Array())
+		child.initialize([(lanes[0] as Node2D).position, (lanes[1] as Node2D).position, 
+			(lanes[2] as Node2D).position], obstacles, lava_obstacles)
 		child.position.x = lane_width * (i + 1)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if GameState.game_ended:
+		return
 	for part: EndlessLevelPart in level_parts:
 		part.position.x -= speed * delta
 	_spawn_new_part()
-	if !GameState.game_ended:
-		speed += acceleration * delta
+	speed += acceleration * delta
+	
+	if !storm_brewing:
+		storm_brewing = GameState.random.randf() <= 0.5
+		if storm_brewing:
+			print("A storm is brewing")
+			storm_timer_seconds = -pre_storm_duration_seconds
+	else:
+		previous_storm_timer_seconds = storm_timer_seconds
+		storm_timer_seconds += delta
+		if previous_storm_timer_seconds < 0 && storm_timer_seconds >=0:
+			_begin_storm()
+		if storm_timer_seconds >= storm_duration_seconds && previous_storm_timer_seconds < storm_duration_seconds:
+			_end_storm()
+		if storm_timer_seconds >= storm_duration_seconds + pre_storm_duration_seconds:
+			print("Storm has subsided")
+			storm_brewing = false
+	pass
+	
+func _begin_storm():
+	print("Storm has started")
+	for p in level_parts:
+		var part = p as EndlessLevelPart
+		if part.position.x >= lane_width:
+			part.spawn_lava_nodes(lava_obstacles)
+	pass
+	
+func _end_storm():
+	print("Storm has ended")
+	for p in level_parts:
+		var part = p as EndlessLevelPart
+		part.remove_lava_nodes()
 	pass
 	
 func _rotate_parts_array():
@@ -49,10 +91,12 @@ func _spawn_new_part():
 	if (level_parts[0] as EndlessLevelPart).position.x > -lane_width * parts_buffer:
 		return
 	_rotate_parts_array()
+	(level_parts.back() as EndlessLevelPart).queue_free()
 	level_parts.pop_back()
 	var child = background.instantiate() as EndlessLevelPart
 	add_child(child)
 	child.position.x = (level_parts.back() as EndlessLevelPart).position.x + lane_width
-	child.initialize([(lanes[0] as Node2D).position, (lanes[1] as Node2D).position, (lanes[2] as Node2D).position], obstacles, Array())
+	child.initialize([(lanes[0] as Node2D).position, (lanes[1] as Node2D).position, 
+	(lanes[2] as Node2D).position], obstacles, lava_obstacles, storm_in_progress)
 
 	level_parts.push_back(child)
--- brackeys_jam_2024/Levels/endless_level_part.gd
+++ brackeys_jam_2024/Levels/endless_level_part.gd
@@ -11,7 +11,6 @@ var _cells : Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_safe_path.resize(_cell_count)
 	_cells.resize(3 * _cell_count)
 	pass # Replace with function body.
 
@@ -23,15 +22,17 @@ func initialize_positions_only(positions: Array):
 func initialize(positions: Array, obstacles: Array, lava_nodes: Array, spawn_lava_nodes: bool = false):
 	initialize_positions_only(positions)
 	
+	_safe_path.resize(_cell_count)
 	for i in _cells.size():
 		var random_value = GameState.random.randf()
-		if random_value <= 0.2:
+		if random_value <= 1 - ProjectSettings.get_setting("global/obstalce_spawn_chance"):
 			continue
 		_cells[i] = obstacles[GameState.random.randi_range(0, obstacles.size() - 1)]
 	
 	for i in _safe_path.size():
 		_safe_path[i] = GameState.random.randi_range(0, 2)
 		_set_cell(_safe_path[i], i, null)
+		assert(_get_cell(_safe_path[i], i) == null)
 		
 	for i in _cells.size():
 		if _cells[i] == null:
@@ -40,8 +41,33 @@ func initialize(positions: Array, obstacles: Array, lava_nodes: Array, spawn_lav
 		var lane = i / _cell_count
 		child.collision_layer |= 1 << lane
 		_get_lane(lane).add_child(child)
-		child.position = Vector2((_lane_width / _cell_count) * (i % _cell_count) + (_lane_width / _cell_count) / 2, 0)
+		child.position = Vector2((_lane_width / _cell_count) * 
+			(i % _cell_count) + (_lane_width / _cell_count) / 2, 0)
 	
+	if spawn_lava_nodes:
+		spawn_lava_nodes(lava_nodes)
+	
+	
+func spawn_lava_nodes(lava_nodes: Array):
+		for i in _cells.size():
+			if _cells[i] != null:
+				continue
+			if _is_safe_cell(i):
+				continue
+			_cells[i] = lava_nodes[0];
+			var child: = (_cells[i] as PackedScene).instantiate() as Obstacle
+			var lane = i / _cell_count
+			child.collision_layer |= 1 << lane
+			_get_lane(lane).add_child(child)
+			child.position = Vector2((_lane_width / _cell_count) * (i % _cell_count) + (_lane_width / _cell_count) / 2, 0)
+
+func remove_lava_nodes():
+	for i in _lane_proxies.size():
+		for child in _get_lane(i).get_children():
+			if child.get_meta("type") == "lava":
+				print("lava found")
+				child.queue_free()
+		
 
 func _get_lane(index: int) -> Node2D:
 	return get_node(_lane_proxies[index])
@@ -51,10 +77,28 @@ func _set_cell(lane: int, index: int, scene: PackedScene):
 	
 func _get_cell(lane: int, index: int) -> PackedScene:
 	return _cells[lane * _cell_count + index]
+	
+func _is_safe_cell(index: int) -> bool:
+	var lane = index / _cell_count;
+	return _cells[index] == null && _safe_path[index % _cell_count] == lane
 
+func _draw() -> void:
+	#for i in _safe_path.size():
+		#draw_string(ThemeDB.fallback_font, _cell_to_position(_safe_path[i], i), "Is Safe", HORIZONTAL_ALIGNMENT_CENTER)
+	pass
 
+func _cell_to_lane(index: int) -> int:
+	return index / _cell_count
+	
+func _cell_to_column(index: int) -> int:
+	return index % _cell_count
+
+func _cell_to_position_cell_only(index: int) ->Vector2:
+	return _cell_to_position(_cell_to_lane(index), _cell_to_column(index))
+
+func _cell_to_position(lane: int, column: int) ->Vector2:
+	return Vector2((_lane_width / _cell_count) * column + (_lane_width / _cell_count) / 2, _get_lane(lane).global_position.y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Sprite2D.visible = !GameState.game_ended
 	pass
