fb46dfd6ba845daa6a15272fc7d8bced40734558
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2024-09-12 20:07:40 -0400

    - Lightning animation is now in along with a flash

--- brackeys_jam_2024/Levels/endless_level_generator.gd
+++ brackeys_jam_2024/Levels/endless_level_generator.gd
@@ -8,9 +8,12 @@ extends Node2D
 @export var initial_speed: float = 300
 @export var max_speed: float = 600
 @export var acceleration: float = 10
+
+@onready var background_container:= $Background
 @onready var sky:= $Sky/Background
 @onready var clouds:= $Sky/Clouds
 @onready var rain:= $Sky/Rain
+@onready var flash:= $Flash
 
 var speed: float
 var level_parts: Array
@@ -19,6 +22,7 @@ const parts_buffer : int = 2
 
 var storm_brewing: bool = false
 var storm_timer_seconds: float
+var lightning_timer: float
 var previous_storm_timer_seconds: float
 @export var pre_storm_duration_seconds: float = 5
 @export var storm_duration_seconds: float = 15
@@ -32,12 +36,12 @@ func _ready() -> void:
 	speed = initial_speed
 	
 	var child = background.instantiate() as EndlessLevelPart
-	add_child(child)
+	background_container.add_child(child)
 	level_parts.push_back(child)
 	
 	for i in parts_buffer * 2:
 		child = background.instantiate() as EndlessLevelPart
-		add_child(child)
+		background_container.add_child(child)
 		level_parts.push_back(child)
 		child.initialize([(lanes[0] as Node2D).position, (lanes[1] as Node2D).position, 
 			(lanes[2] as Node2D).position], obstacles, lava_obstacles)
@@ -67,6 +71,9 @@ func _process(delta: float) -> void:
 		sky.speed_scale = 1.0 / pre_storm_duration_seconds
 		clouds.speed_scale = 1.0 / pre_storm_duration_seconds
 	
+	if storm_in_progress:
+		lightning_timer += delta
+	
 	if storm_brewing:
 		previous_storm_timer_seconds = storm_timer_seconds
 		storm_timer_seconds += delta
@@ -80,18 +87,30 @@ func _process(delta: float) -> void:
 			clouds.play("inactive")
 			storm_brewing = false
 			storm_brewing_timer.start()
+		if lightning_timer >= 3:
+			_play_lightning()
 	pass
 	
 func _begin_storm():
 	print("Storm has started")
-	sky.play("active")
 	clouds.play("active")
 	rain.visible = true
+	_play_lightning()
 	for p in level_parts:
 		var part = p as EndlessLevelPart
 		if part.position.x >= lane_width:
 			part.spawn_lava_nodes(lava_obstacles)
 	pass
+	
+	_full_screen_flash()
+	
+func _play_lightning():
+	lightning_timer = 0
+	sky.play("lightning")
+	sky.speed_scale = 1.0
+	await sky.animation_finished
+	sky.play("active")
+	sky.speed_scale = 1.0 / pre_storm_duration_seconds
 	
 func _end_storm():
 	print("Storm has ended")
@@ -104,6 +123,18 @@ func _end_storm():
 		var part = p as EndlessLevelPart
 		part.remove_lava_nodes()
 	pass
+	
+	_full_screen_flash()
+	
+func _full_screen_flash():
+	flash.visible = true
+	await get_tree().create_timer(0.1).timeout
+	flash.visible = false
+	await get_tree().create_timer(0.05).timeout
+	flash.visible = true
+	await get_tree().create_timer(0.05).timeout
+	flash.visible = false
+	
 	
 func _rotate_parts_array():
 	for i in level_parts.size() - 1:
@@ -118,7 +149,7 @@ func _spawn_new_part():
 	(level_parts.back() as EndlessLevelPart).queue_free()
 	level_parts.pop_back()
 	var child = background.instantiate() as EndlessLevelPart
-	add_child(child)
+	background_container.add_child(child)
 	child.position.x = (level_parts.back() as EndlessLevelPart).position.x + lane_width
 	child.initialize([(lanes[0] as Node2D).position, (lanes[1] as Node2D).position, 
 	(lanes[2] as Node2D).position], obstacles, lava_obstacles, storm_in_progress)
